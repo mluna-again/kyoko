c54e0369d7642561acfdf549c39118ddd9b32454
Manuel Luna <manuellunada@gmail.com>
2024-04-16 19:58:59 -0600

    remote mailbox

--- lib/kyoko_web/router.ex
+++ lib/kyoko_web/router.ex
@@ -35,16 +35,4 @@ defmodule KyokoWeb.Router do
       live_dashboard "/dashboard", metrics: KyokoWeb.Telemetry
     end
   end
-
-  # Enables the Swoosh mailbox preview in development.
-  #
-  # Note that preview only shows emails that were sent by the same
-  # node running the Phoenix server.
-  if Mix.env() == :dev do
-    scope "/dev" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
-    end
-  end
 end
